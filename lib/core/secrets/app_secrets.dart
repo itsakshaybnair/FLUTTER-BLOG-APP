c9ce0152b91c9cbd46c453a9c0391bffed307f52
Akshay B Nair <itsakshaybnair@gmail.com>
2025-03-27 23:39:10 +0530

    third commit

--- lib/core/secrets/app_secrets.dart
+++
@@ -1,7 +0,0 @@
-class AppSecrets {
-  // static const supabaseUrl = 'https://axprhujhofspjuudhimd.supabase.co';
-  static const supabaseUrl = 'https://uhrhmpemxouwzyhzhcpj.supabase.co';
-  //static const supabaseAnonkey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4cHJodWpob2ZzcGp1dWRoaW1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU1NTMyNjgsImV4cCI6MjA0MTEyOTI2OH0.bClEIYctkTImThOIOZxIGVtelONXSd08np5xo9yTItk';
-  static const supabaseAnonkey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVocmhtcGVteG91d3p5aHpoY3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwOTkyNjgsImV4cCI6MjA1NzY3NTI2OH0.BsmbHzKwIA8VvaJHk_s32_JYSfXbSu8Q5r3yzS0H_PU';
-}
